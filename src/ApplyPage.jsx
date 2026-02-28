0d0b846215ca4dc8e8031bc69a8f67082645f007
Sadock Chakma <200020038@iitdh.ac.in>
2026-02-28 19:28:22 +0530

    .

--- src/ApplyPage.jsx
+++ src/ApplyPage.jsx
@@ -17,7 +17,7 @@ function ApplyPage({ jobId, onBack }) {
 
     try {
       // Send the POST request to your FastAPI backend
-     const response = await fetch('https://dummy-job-portal-backend.onrender.com', {
+     const response = await fetch('https://dummy-job-portal-backend.onrender.com/docs', {
         method: 'POST',
         body: formData,
       });
